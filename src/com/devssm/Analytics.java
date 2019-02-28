111a3d140697ff40e1f418874b9add3a13cd485a
Sebastian Soto Madrigal <dev.snsm@gmail.com>
2019-02-27 19:54:23 -0600

    Se pasa la prueba #5, retorna solo la primer palabra mas larga. Se hacen cambios a masLarga y masCorta respecto a las pruebas #3 y #4

--- src/com/devssm/Analytics.java
+++ src/com/devssm/Analytics.java
@@ -3,22 +3,10 @@ package com.devssm;
 import java.util.*;
 
 public class Analytics {
-    public static String[] masLarga(String sentence) {
-        String [] words = sentence.split(" |,");
-        ArrayList<String> longWords = new ArrayList<>();
-        String longest = "";
-        for (int i = 0; i < words.length; i++) {
-            if(words[i].length() > longest.length()){
-                longest = words[i];
-            }
-        }
-        longWords.add(longest);
-        for (int i = 0; i < words.length; i++) {
-            if(words[i].length() == longest.length() && !longest.equals(words[i])){
-                longWords.add(words[i]);
-            }
-        }
-        return ordenarListaEnArreglo(longWords);
+    public static String[] masLargaOrdenada(String sentence) {
+        ArrayList<String>lista = masLarga(sentence);
+        Collections.sort(lista);
+        return fromListToArray(lista);
     }
     
     public static String[] masCorta(String sentence){
@@ -36,11 +24,33 @@ public class Analytics {
                 shortWords.add(words[i]);
             }
         }
-        return ordenarListaEnArreglo(shortWords);
+        Collections.sort(shortWords);
+        return fromListToArray(shortWords);
     }
 
-    public static String [] ordenarListaEnArreglo(ArrayList<String> lista){
-        Collections.sort(lista);
+    public static ArrayList<String> masLarga(String sentence){
+        String [] words = sentence.split(" |,");
+        ArrayList<String> longWords = new ArrayList<>();
+        String longest = "";
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length() > longest.length()){
+                longest = words[i];
+            }
+        }
+        longWords.add(longest);
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length() == longest.length() && !longest.equals(words[i])){
+                longWords.add(words[i]);
+            }
+        }
+        return longWords;
+    }
+
+    public static String primeraMasLarga(String sentence){
+        return fromListToArray(masLarga(sentence))[0];
+    }
+
+    public static String [] fromListToArray(ArrayList<String> lista){
         return lista.toArray(new String[lista.size()]);
     }
 }
--- src/test/com/devssm/TestAnalytics.java
+++ src/test/com/devssm/TestAnalytics.java
@@ -3,8 +3,6 @@ package test.com.devssm;
 import com.devssm.Analytics;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAnalytics {
@@ -14,8 +12,8 @@ public class TestAnalytics {
         String [] res1 = {"casototota"};
         String [] res2 = {"casa","cosa"};
 
-        String [] val1 = Analytics.masLarga("casa casita casototota");
-        String [] val2 = Analytics.masLarga("casa,mia,cosa");
+        String [] val1 = Analytics.masLargaOrdenada("casa casita casototota");
+        String [] val2 = Analytics.masLargaOrdenada("casa,mia,cosa");
 
         assertEquals(res1[0], val1[0],"Should return casototota");
 
@@ -40,7 +38,12 @@ public class TestAnalytics {
 
     @Test
     public void testPrimeraMasLarga(){
+        String res1 = "casototota",res2 = "cosa";
 
+        String val1 = Analytics.primeraMasLarga("casototota casototato"),val2 = Analytics.primeraMasLarga("cosa casa");
+
+        assertEquals(res1,val1,"Should return casototota");
+        assertEquals(res2,val2,"Should return cosa");
     }
 
 }
